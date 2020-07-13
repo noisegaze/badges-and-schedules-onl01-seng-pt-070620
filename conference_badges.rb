8c59e4f28a1c0110d70bde208d1346b7c9dd5638
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-13 21:45:51 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+
+
+def batch_badge_creator(attendees)
+  badge_messages = []
+  attendees.each do |name|
+    badge_messages << badge_maker(name)
+  end 
+   badge_messages
+end
+
+
+
+def assign_rooms(attendees)
+    attendees.each_with_index.map do |name, index|
+  "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end   
+
+def printer(attendees)
+   batch_badge_creator(attendees).each do |badge|
+     puts badge
+   end
+   assign_rooms(attendees).each do |rooms|
+      puts rooms
+    end  
+end  
+  
+  
+  
+  
+  
+  
+  
+
